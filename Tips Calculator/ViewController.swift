da2c80de0136940b8588cae8e241b810ca040042
Tam Nguyen <tnguyen@smith.edu>
2015-12-28 17:53:28 -0500

    Initial Commit

---
+++ Tips Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tips Calculator
+//
+//  Created by Tam Nguyen on 12/28/15.
+//  Copyright © 2015 Tam Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
